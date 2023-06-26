6e37f6c388805fc6941c7f6e0dd09b907db5127e
Domme19 <mabdieuveille@gmail.com>
2023-06-26 18:48:44 +0200

    navigate button added

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,17 @@
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import {useNavigation} from "@react-navigation/native";
 export default function HomeScreen (){
+    const navigation = useNavigation();
     return(
         <View style={styles.container}>
-            <Text style={{fontSize: 20}}>
+            <Text style={{fontSize: 20, marginBottom: 10}}>
                 Home screen
             </Text>
+            <TouchableOpacity style={styles.navigateBtn} onPress={() => navigation.navigate("Login1" as never)}>
+                <Text style={{textAlign: 'center', color: 'white', fontSize: 16}}>
+                    Navigate to Login
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -12,8 +19,16 @@ export default function HomeScreen (){
 const styles = StyleSheet.create({
     container:{
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor:'lightblue',
-        paddingTop: 40,
-        paddingLeft: 20
+    },
+
+    navigateBtn:{
+        backgroundColor: 'blue',
+        paddingHorizontal: 10,
+        paddingVertical: 15,
+        width: '60%',
+        borderRadius: 10
     }
 })
