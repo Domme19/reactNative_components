4db609801c5efb901ca77a871ab07afa7604e213
Domme19 <mabdieuveille@gmail.com>
2023-06-26 17:04:16 +0200

    fixed navigation

--- App.tsx
+++ App.tsx
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Navigation from "./navigation";
 import {useColorScheme} from "react-native";
 
 export default function App() {
   const colorScheme = useColorScheme()
-  return (
-    <View style={styles.container}>
-      <Navigation colorScheme={colorScheme} />
-    </View>
-  );
+  return(
+      <Navigation colorScheme={colorScheme}/>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
