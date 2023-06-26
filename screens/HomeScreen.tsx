bb02bc293ed31cf6635e5699ef8fc4475b4c93f0
Domme19 <mabdieuveille@gmail.com>
2023-06-26 17:05:43 +0200

    testing colors

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,17 @@
-import {View, Text} from "react-native";
-
+import {View, Text, StyleSheet} from "react-native";
 export default function HomeScreen (){
     return(
-        <View>
+        <View style={styles.container}>
             <Text>
                 Home screen
             </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:'lightblue'
+    }
+})
